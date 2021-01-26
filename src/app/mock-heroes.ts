dd7722d66958f956b356a67be18f56c0ae0b99a5
dumadoniagara <dumadsambora@gmail.com>
2021-01-26 22:08:09 +0700

    looping element use *ngFor

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+   { id: 1, name: 'Dr Nice' },
+   { id: 2, name: 'The Flash' },
+   { id: 3, name: 'Dr Strange' },
+   { id: 4, name: 'Captain America' },
+   { id: 5, name: 'Captain Marvel' },
+   { id: 6, name: 'Iron Man' },
+   { id: 7, name: 'Magma' },
+]
